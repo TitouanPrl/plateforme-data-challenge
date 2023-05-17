b3bf3a05b91300014e80a7fb5a35ad4439a7fb4a
ClemAMolette <clementinegaga79@outlook.fr>
2023-05-17 15:36:34 +0200

    finalisation structure bdd

--- sql/siteprojet.sql
+++ sql/siteprojet.sql
@@ -5,72 +5,89 @@ USE SiteWeb;
 
 
 CREATE TABLE Utilisateur(
-	idUser INT PRIMARY KEY,
+	idUser INTEGER(3) PRIMARY KEY auto_increment,
 	nom VARCHAR(30),
     prenom VARCHAR(30),
     entreprise VARCHAR(30),
-    numTel VARCHAR(13),
+    numTel VARCHAR(16),
     email VARCHAR(250),
     mdp VARCHAR(30)
 );
 
 CREATE TABLE Equipe(
-    idEquipe INT PRIMARY KEY,
-    capitaine INT,
-    m2 INT DEFAULT NULL, -- membre de l'équipe numéro 2
-    m3 INT DEFAULT NULL,
-    m4 INT DEFAULT NULL,
-    m5 INT DEFAULT NULL,
-    m6 INT DEFAULT NULL,
-    m7 INT DEFAULT NULL,
-    m8 INT DEFAULT NULL
+    idEquipe INTEGER(3) PRIMARY KEY auto_increment,
+    capitaine INTEGER(3),
+    m2 INTEGER(3) DEFAULT NULL, -- membre de l'équipe numéro 2
+    m3 INTEGER(3) DEFAULT NULL,
+    m4 INTEGER(3) DEFAULT NULL,
+    m5 INTEGER(3) DEFAULT NULL,
+    m6 INTEGER(3) DEFAULT NULL,
+    m7 INTEGER(3) DEFAULT NULL,
+    m8 INTEGER(3) DEFAULT NULL
 );
 
 CREATE TABLE Message (
-    idMessage INT PRIMARY KEY,
+    idMessage INTEGER(3) PRIMARY KEY auto_increment,
     contenu VARCHAR(250),
-    idExpediteur INT,
-    idDestinataire INT
+    idExpediteur INTEGER(3),
+    idDestinataire INTEGER(3)
 );
 
 CREATE TABLE Questionnaire (
-    idQuestionnaire INT PRIMARY KEY,
-    lienForm VARCHAR(250)
+    idQuestionnaire INTEGER(3) PRIMARY KEY,
+    idSujet INTEGER(3),
+    FOREIGN KEY (idSujet) REFERENCES Sujet (idSujet)
+);
+
+CREATE TABLE Question (
+    idQuestion INTEGER(3) PRIMARY KEY,
+    contenu VARCHAR(250),
+    idQuestionnaire INTEGER(3),
+    FOREIGN KEY (idQuestionnaire) REFERENCES Questionnaire (idQuestionnaire)
+);
+
+CREATE TABLE Réponse (
+    idReponse INTEGER(3) PRIMARY KEY,
+    contenu VARCHAR(1024),
+    idQuestion INTEGER(3),
+    idEquipe INTEGER(3),
+    FOREIGN KEY (idQuestion) REFERENCES Question (idQuestion),
+    FOREIGN KEY (idEquipe) REFERENCES Equipe (idEquipe)
 );
 
 CREATE TABLE Podium (
-    idPodium INT PRIMARY KEY,
-    idE1 INT DEFAULT NULL,
-    idE2 INT DEFAULT NULL,
-    idE3 INT DEFAULT NULL,
-    idSujet INT,
-    FOREIGN KEY (idSujet) REFERENCES ProjetData (idSujet)
+    idPodium INTEGER(3) PRIMARY KEY,
+    idE1 INTEGER(3) DEFAULT NULL,
+    idE2 INTEGER(3) DEFAULT NULL,
+    idE3 INTEGER(3) DEFAULT NULL,
+    idSujet INTEGER(3),
+    FOREIGN KEY (idSujet) REFERENCES Sujet (idSujet)
 
 );
 
-CREATE TABLE ProjetEtudiant (
-    idProjet INT PRIMARY KEY,
-    idEquipe INT,
-    idSujet int,
+CREATE TABLE Projet (
+    idProjet INTEGER(3) PRIMARY KEY,
+    idEquipe INTEGER(3),
+    idSujet INTEGER(3),
     lienCode VARCHAR(250),
     FOREIGN KEY (idEquipe) REFERENCES Equipe (idEquipe),
-    FOREIGN KEY (idUser) REFERENCES ProjetData (idSujet)
+    FOREIGN KEY (idUser) REFERENCES Sujet (idSujet)
 );
 
-CREATE TABLE ProjetData (
-    idSujet INT PRIMARY KEY,
-    idChallenge INT,
+CREATE TABLE Sujet (
+    idSujet INTEGER(3) PRIMARY KEY,
+    idEvenement INTEGER(3),
     libelle VARCHAR(30),
     descrip VARCHAR(250),
     img VARCHAR(250),
-    telGerant INT,
+    telGerant VARCHAR(16),
     emailGerant VARCHAR(250),
     lienResources VARCHAR(250),
-    FOREIGN KEY (idChallenge) REFERENCES DataChallenge (idChallenge)
+    FOREIGN KEY (idEvenement) REFERENCES Evenement (idEvenement)
 );
 
-CREATE TABLE DataChallenge (
-    idChallenge INT PRIMARY KEY,
+CREATE TABLE Evenement (
+    idEvenement INT PRIMARY KEY,
     libelle VARCHAR(30),
     dateD DATETIME,
     dateF DATETIME
