a634a75afb67208d2b57d5b62b219d6891b0ee8d
ClemAMolette <clementinegaga79@outlook.fr>
2023-05-22 10:15:14 +0200

    fct delete/add dans bdd + delete on cascade

--- bdd/fonctionsBDD.php
+++ bdd/fonctionsBDD.php
@@ -20,16 +20,87 @@ function disconnect($conn) {
     $conn->close();
 }
 
+function request($conn,$sql) {
+    $result = mysqli_query($conn, $sql);
+    while ($ligne = $result->fetch_assoc()) {
+        $tableau[] = $ligne;
+    }
+    return $tableau;
+}
 
 // On récupère les données d'une catégorie en fonction de l'id
 function getPersonById($conn,$id) {
     $sql = "SELECT * FROM Utilisateur WHERE idUser=$id";
-    $result = mysqli_query($conn, $sql);
-    $person = mysqli_fetch_assoc($result);
+    $person = request($conn,$sql);
     return $person;
-    
 }
 
+function getUserByType($conn,$fonction) {
+    $sql = "SELECT * FROM Utilisateur WHERE fonction=$fonction";
+    $people = request($conn,$sql);
+    return $people;
+}
+
+function getAllUsers($conn) {
+    $etudiants = getUserByType($conn,'USER');
+    $gestion = getUserByType($conn,'GESTION');
+    $admin = getUserByType($conn,'ADMIN');
+    $users = array();
+    $users[] = $etudiants;
+    $users[] = $gestion;
+    $users[] = $admin;
+
+    return $users;
+}
+
+function getPodiumByIDSujet($conn, $idSujet) {
+    $sql = "SELECT * FROM Podium WHERE idSujet=$idSujet";
+    $podium = request($conn,$sql);
+
+    return $podium;
+}
+
+
+//AJOUT D'UTILISATEURS
+function addAdmin($nom,$prenom,$numTel,$email,$mdp) {
+    $sql = "INSERT INTO Utilisateur (nom,prenom,numTel,email,mdp,fonction) VALUES ($nom,$prenom,,$numTel,$email,$mdp,'ADMIN')";
+    mysqli_connect($conn,$sql);
+}
+function addGestion($nom,$prenom,$entreprise,$numTel,$email,$mdp,$dateD) {
+    $sql = "INSERT INTO Utilisateur (nom,prenom,entreprise,numTel,email,mdp,dateD,fonction) VALUES ($nom,$prenom,$entreprise,$numTel,$email,$mdp,$dateD,'GESTION')";
+    mysqli_connect($conn,$sql);
+}
+function addEtudiant($nom,$prenom,$numTel,$email,$mdp,$nivEtude,$ecole,$ville) {
+    $sql = "INSERT INTO Utilisateur (nom,prenom,numTel,email,mdp,nivEtude,ville,ecole,fonction) VALUES ($nom,$prenom,$numTel,$email,$mdp,$nivEtude,$ville,$ecole,'USER')";
+    mysqli_connect($conn,$sql);
+}
+
+
+//SUPPRESSION DE DONNÉES
+function deleteUser($idUser) {
+    $sql = "DELETE FROM Utilisateur WHERE idUser = $idUser";
+    mysqli_connect($conn,$sql);
+}
+function deleteSujet($idSujet) {
+    $sql = "DELETE FROM Sujet WHERE idSujet = $idSujet";
+    mysqli_connect($conn,$sql);
+}
+function deleteEvenement($idEvenement) {
+    $sql = "DELETE FROM Evenement WHERE idEvenement = $idEvenement";
+    mysqli_connect($conn,$sql);
+}
+function deleteProjet($idProjet) {
+    $sql = "DELETE FROM Projet WHERE idProjet = $idProjet";
+    mysqli_connect($conn,$sql);
+}
+function deletePodium($idPodium) {
+    $sql = "DELETE FROM Podium WHERE idPodium = $idPodium";
+    mysqli_connect($conn,$sql);
+}
+
+
+//recup tous les sujets par challenge
+//recup tous les challenge
 
 
 
--- sql/siteprojet.sql
+++ sql/siteprojet.sql
@@ -11,7 +11,12 @@ CREATE TABLE Utilisateur(
     entreprise VARCHAR(30),
     numTel VARCHAR(16),
     email VARCHAR(250),
+    nivEtude VARCHAR(16),
+    ecole VARCHAR(64),
+    ville VARCHAR(64),
     mdp VARCHAR(256),
+    dateD DATETIME,
+    dateF DATETIME,
     fonction VARCHAR(16)   /* ADMIN/GESTION/USER */
 );
 
@@ -51,7 +56,7 @@ CREATE TABLE Sujet (
     telGerant VARCHAR(16),
     emailGerant VARCHAR(250),
     lienResources VARCHAR(250),
-    FOREIGN KEY (idEvenement) REFERENCES Evenement (idEvenement)
+    FOREIGN KEY (idEvenement) REFERENCES Evenement (idEvenement) on delete cascade
 );
 
 CREATE TABLE Projet (
@@ -59,8 +64,8 @@ CREATE TABLE Projet (
     idEquipe INTEGER,
     idSujet INTEGER,
     lienCode VARCHAR(250),
-    FOREIGN KEY (idEquipe) REFERENCES Equipe (idEquipe),
-    FOREIGN KEY (idSujet) REFERENCES Sujet (idSujet)
+    FOREIGN KEY (idEquipe) REFERENCES Equipe (idEquipe) on delete cascade,
+    FOREIGN KEY (idSujet) REFERENCES Sujet (idSujet) on delete cascade
 );
 
 CREATE TABLE Podium (
@@ -69,21 +74,21 @@ CREATE TABLE Podium (
     idE2 INTEGER DEFAULT NULL,
     idE3 INTEGER DEFAULT NULL,
     idSujet INTEGER,
-    FOREIGN KEY (idSujet) REFERENCES Sujet (idSujet)
+    FOREIGN KEY (idSujet) REFERENCES Sujet (idSujet) on delete cascade
 
 );
 
 CREATE TABLE Questionnaire (
     idQuestionnaire INTEGER PRIMARY KEY,
     idSujet INTEGER,
-    FOREIGN KEY (idSujet) REFERENCES Sujet (idSujet)
+    FOREIGN KEY (idSujet) REFERENCES Sujet (idSujet) on delete cascade
 );
 
 CREATE TABLE Question (
     idQuestion INTEGER PRIMARY KEY,
     contenu VARCHAR(250),
     idQuestionnaire INTEGER,
-    FOREIGN KEY (idQuestionnaire) REFERENCES Questionnaire (idQuestionnaire)
+    FOREIGN KEY (idQuestionnaire) REFERENCES Questionnaire (idQuestionnaire) on delete cascade
 );
 
 CREATE TABLE Réponse (
@@ -91,8 +96,8 @@ CREATE TABLE Réponse (
     contenu VARCHAR(1024),
     idQuestion INTEGER,
     idEquipe INTEGER,
-    FOREIGN KEY (idQuestion) REFERENCES Question (idQuestion),
-    FOREIGN KEY (idEquipe) REFERENCES Equipe (idEquipe)
+    FOREIGN KEY (idQuestion) REFERENCES Question (idQuestion) on delete cascade,
+    FOREIGN KEY (idEquipe) REFERENCES Equipe (idEquipe) on delete cascade
 );
 
 
