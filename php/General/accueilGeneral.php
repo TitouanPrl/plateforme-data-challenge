03cf444b11f4581066da046e361e42c7b21dc0b6
DEPRETER-Remi <depreterremi@gmail.com>
2023-05-22 09:17:56 +0200

    IA PAU

--- php/General/accueilGeneral.php
+++ php/General/accueilGeneral.php
@@ -3,7 +3,7 @@
 <!-- MAIN CONTENT -->
 
 <main>
-    <h1>pas ia PAU</h1>
+    <h1>IA PAU</h1>
     <h2>L'intelligence artificielle vue des Pyrénées</h2>
 
     <!-- RAJOUTER 2-3 TXT SINON CA FAIT VIDE -->
