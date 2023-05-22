fcdce5031e425f209b7afa6361a9f18f8ae90d1b
Naiwann-Lallinec <naiwann.l29@gmail.com>
2023-05-22 14:16:18 +0200

    changements

--- java/Nombres.java
+++ java/Nombres.java
@@ -36,7 +36,6 @@ public class Nombres {
 	    
 	    for (String ligne : lignes) {
 	    	if (ligne.trim().startsWith("\"\"\"")) {
-	    		System.out.println("ligne commence par les guillemets dont le state est :" + state);
 	    		if (ligne.trim().endsWith("\"\"\"") && ligne.trim().indexOf("\"\"\"",3) != -1) {
 	    			sb.append("\n");
 	    		}
@@ -45,35 +44,37 @@ public class Nombres {
 	    		}
 	    		else {
 	    			sb.append("\n");
-	    			System.out.println();
-	    			state = true;
-	    		}
-	    	}
-	    	else if (ligne.trim().contains("\"\"\"")) {
-	    		System.out.println("ligne contient des guillemets mais ne commence pas par ceux ci dont le state est : " + state );
-	    		if (state = true) {
-	    			state = false;
-	    			if (ligne.trim().endsWith("\"\"\"")) {
-	    				sb.append("\n");
+	    			if (state == true) {
+	    				state = false;
 	    			}
 	    			else {
 	    				state = true;
-	    				sb.append(ligne).append("\n");;
+	    			}
+	    		}
+	    	}
+	    	else if (ligne.trim().contains("\"\"\"")) {
+	    		if (state == true) {
+	    			state = false;
+	    			int indexg = ligne.trim().indexOf("\"\"\"");
+	    			if (ligne.trim().indexOf("\"\"\"",indexg+1) != -1) {
+	    				sb.append("\n");
+	    			}
+	    			else {
+	    				sb.append(ligne).append("\n");
 	    			}
 	    		}
 	    		else {
-	    			sb.append(ligne);	
+	    			state = true;
+	    			sb.append(ligne).append("\n");	
 	    		}
 	    	}
 	    	else {
-	    		System.out.println("ligne sans commentaires dont le state est : " + state);
 	    		if (state == true) {
 	    			sb.append("\n");
 	    		}
 	    		else {
 		    		sb.append(ligne).append("\n");	    			
 	    		}
-
 	    	}
 	    }
 	    return sb.toString();
@@ -94,16 +95,95 @@ public class Nombres {
     }
 	
 	public static String supprimerCommentaires(String texte) {
-		String texteS = supprimerHastags(texte);
+		String texteG = supprimerTriplesQuotes(texte);
+		String texteS = supprimerHastags(texteG);
 		texteS = supprimerLignesVides(texteS);
 		return texteS;
 	}
 	
+	public static int nbLignes(String texte) {
+		String[] lignes = texte.split("\\r?\\n");
+		int nbl = 0;
+		for (String ligne : lignes) {
+			nbl += 1;
+		}
+		return nbl;
+	}
+	
+	public static int nbFonction(String texte) {
+		String[] lignes = texte.split("\\r?\\n");
+		int nbf = 0;
+		String tabulation = "";
+		boolean dansf = false;
+		for (String ligne : lignes) {
+			if (ligne.trim().startsWith("def") && dansf == false) {
+				dansf = true;
+				nbf += 1;
+				tabulation += "";
+			}
+			else if (dansf == true) {
+				if (ligne.trim().startsWith(tabulation)) {
+					dansf = false;
+					tabulation = "";
+				}
+				else {
+					nbf += 1 ;
+					if (ligne.trim().startsWith("\t")) {
+						tabulation += "";
+					}
+				}
+			}
+		}		
+		return nbf;
+	}
+	
+	public static void Fonctions(String texte) {
+		String[] lignes = texte.split("\\r?\\n");
+		int min = -1;
+		int max = -1;
+		int somme = 0;
+		int nbl = 0;
+		String tabulation = "";
+		boolean dansf = false;
+		for (String ligne : lignes) {
+			if (ligne.trim().startsWith("def")) {
+				dansf = true;
+				tabulation += " ";
+			}
+			else if (dansf == true) {
+				nbl += 1;
+				if (!ligne.trim().startsWith(tabulation)) {
+					dansf= false;
+					tabulation = "";
+					somme += nbl;
+					if (min == -1) {
+						min = nbl;
+					}
+					if (max == -1) {
+						max = nbl;
+					}
+					if (nbl > max) {
+						max = nbl;
+					}
+					if (nbl < min) {
+						min = nbl;
+					}
+					nbl = 0;
+				}
+			}
+			int moy = somme/nbFonction(texte);
+			System.out.println("le nombre minimum de lignes des fonctions est : " + min);
+			System.out.println("le nombre maximum de lignes des fonction est :" + max);
+			System.out.println(" le nombre moyen de lignes des fonctions est : " + moy);
+		}	
+	}
+	
 	public static void main(String[] args) throws IOException {
-		String chemin = "Home/Desktop/AnalysePY2JSON/Fichiers/python/fichier_python.txt";
+		String chemin = "Fichiers/python/fichier_python.txt";
 		String texte=txtToString(chemin);
-
-	    System.out.println(supprimerTriplesQuotes(texte));
-	    //System.out.println(supprimerCommentaires(texte));
+		texte = supprimerCommentaires(texte);
+		
+		System.out.println("nombre de fonctions : " + nbFonction(texte));
+		System.out.println("nombre de lignes : " + nbLignes(texte));
 	}
 }
