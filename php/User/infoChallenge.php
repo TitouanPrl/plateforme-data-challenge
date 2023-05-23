394c88e63d516b96298bbee16fd7f9f7d7e79337
TitouanPrl <titouan.pradal@gmail.com>
2023-05-23 14:25:08 +0200

    inscription challenge

--- php/User/infoChallenge.php
+++ php/User/infoChallenge.php
@@ -1,4 +1,4 @@
-<?php
+<?php require '../Integrations/header.php';
 session_start();
 
 /* On inclut les fonctions de manipulation de la BDD */
@@ -15,5 +15,24 @@ connect();
 /* On récupère le challenge sélectionné */
 $idEvent = $_GET['challenge'];
 
+/* On récupère les sujets associés au challenge */
+$tabSujets = getSujetByEvenement($idEvent);
 
-?>
+echo('<!-- MAIN CONTENT -->
+
+<main>');
+
+/* Affichage des challenges disponibles */
+foreach ($tabSujets as $current) {
+
+    echo ('  <!-- SUJETS-->
+        <a href="ressourcesSujet.php?sujet=' . $current['idSujet'] . '
+             <div id="sujets">
+                 <h3>' . $current['libelle'] . '</h3>
+             </div>
+        </a>');
+}
+
+    echo('</main>');
+
+require '../Integrations/footer.php'; ?>
