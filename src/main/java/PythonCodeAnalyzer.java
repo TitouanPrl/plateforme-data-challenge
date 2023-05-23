64474d9278f50108f4059241cd443eaee71e744a
Naiwann-Lallinec <naiwann.l29@gmail.com>
2023-05-23 14:58:14 +0200

    ss

--- src/main/java/PythonCodeAnalyzer.java
+++ src/main/java/PythonCodeAnalyzer.java
@@ -1,11 +1,5 @@
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.File;
-import java.util.HashMap;
+import java.io.*;
+import java.util.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
