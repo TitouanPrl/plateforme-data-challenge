1283f8ab4ece6e163b72ba6c65600eca65410d3b
Naiwann-Lallinec <naiwann.l29@gmail.com>
2023-05-24 15:55:12 +0200

    bjbj

---
+++ php/graphe.php
@@ -0,0 +1,14 @@
+<?php
+$json = file_get_contents('../json/info.json');
+$nameFichier= json_decode($json, true);
+
+$_SESSION['donnees']=$donnees;
+
+foreach($_SESSION['donnees'] as $element => $nameFichier){
+    foreach($nameFichier as $nameFonction => $newLigne){
+        echo' nom du fichier:' .$nameFichier. ', nombre de lignes:' .$newLigne;
+    }
+}
+
+
+?>
