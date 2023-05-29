3a040234ea2c079e097e4baf2b2aab0c739a0da7
TitouanPrl <titouan.pradal@gmail.com>
2023-05-29 20:10:12 +0200

    gestion taille équipe

--- php/User/equipe.php
+++ php/User/equipe.php
@@ -14,9 +14,15 @@ connect();
     <div class="corps">
 
         <?php
+
+        /* ================================== *
+        *           CREATION EQUIPE           *
+        * =================================== */
+
         /* Si l'utilisateur n'a pas d'équipe, on affiche les champs et le bouton lui proposant d'en créer une */
         if (!isset($_SESSION['infoUser']['idEquipe'])) {
             echo ('<div id="creer_equipe">
+            <h2 class="titreForm"> Création d\une équipe </h2>
             <input type="text" id="nom_equipe" placeholder="Nom que vous souhaitez donner à votre équipe">
 
             <input type="text" id="nom_challenge" list="liste_challenges" placeholder="Challenge auquel vous souhaitez inscrire votre équipe">
@@ -29,14 +35,29 @@ connect();
                 echo ('<option value="' . $current['libelle'] . '>');
             }
 
-            echo ('</datalist>
-            <button id="creer_equipe" type="button" onclick="createTeam(' . $_SESSION['infoUser']['prenom'] . ' ' . $_SESSION['infoUser']['nom'] . ')">Créer une équipe</button>
+            echo ('</datalist>');
+
+            /* Deuxième membre (après le capitaine) */
+            echo ('<p class="titre_input"> Membre 1 </p>
+            <input type="text" id="partipant2" list="liste_participants" required>');
+
+            /* Troisième membre (après le capitaine) */
+            echo ('<p class="titre_input"> Membre 2 </p>
+            <input type="text" id="partipant3" list="liste_participants" required>');
+
+            echo ('<button id="creer_equipe" type="button" onclick="createTeam(' . $_SESSION['infoUser']['prenom'] . ' ' . $_SESSION['infoUser']['nom'] . ')">Créer une équipe</button>
             </div>');
         }
 
+        /* ================================== *
+        *                EQUIPE               *
+        * =================================== */
+
         /* Affichage des membres de l'équipe */
         echo ('<!-- Affichage des membres de son équipe -->
-        <div id="monEquipe">');
+        <div id="monEquipe">
+        <h2 class="titreForm"> Mon équipe </h2>
+        <p id="legend"> Une équipe doit avoir entre 3 et 8 membres, si vous êtes capitaine, vous pouvez supprimer un membre en cliquant dessus </p>');
         /* On initialise le compteur pour naviguer dans la classe */
         $i = 0;
         foreach ($_SESSION['teamMembers'] as $member) {
@@ -63,6 +84,11 @@ connect();
 
         echo ('</div>');
 
+
+        /* ================================== *
+        *           AJOUT D'UN MEMBRE         *
+        * =================================== */
+
         /* On récupère le challenge auquel l'équipe participe */
         $idEvent = $_SESSION['infoTeam']['idEvent'];
 
@@ -71,6 +97,7 @@ connect();
 
         /* Champ de saisie pour ajouter un membre à l'équipe */
         echo ('<div id="ajout_membre">
+        <p class="titre_input"> Ajouter un membre </p>
         <input type="text" id="partipant" list="liste_participants" required>
 
         <datalist id="liste_participants">');
@@ -87,7 +114,7 @@ connect();
             echo ('<button type="button" id="but_add_member" onclick="addMemberTeam();">Ajouter à l\'équipe</button>');
         }
         echo ('</div>');
-        
+
         ?>
     </div>
 
--- script/gestionEquipe.js
+++ script/gestionEquipe.js
@@ -1,10 +1,22 @@
-/* Création d'une équipe */
+
+/* ================================== *
+*           CREATION EQUIPE           *
+* =================================== */
+
 function CreateTeam(nom_prenom_cap) {
     var xhr = getXHR();
 
     /* On récupère le nom de l'équipe et celui du challenge */
     var nomTeam = document.getElementById("nom_equipe").innerText;
     var challenge = document.getElementById("nom_challenge").innerText;
+
+    /* Var deuxième user */
+    var membre = document.getElementById("participant2").value;
+    var idMembre = document.getElementById("participant2").name;
+
+    /* Var troisième user */
+    var membre = document.getElementById("participant3").value;
+    var idMembre = document.getElementById("participant3").name;
 
     console.log(nomTeam);
     console.log(challenge);
@@ -16,28 +28,62 @@ function CreateTeam(nom_prenom_cap) {
             let myTeam = document.getElementById("monEquipe");
             let formEquipe = document.getElementById("creer_equipe");
 
-            /* === On crée la ligne à ajouter et ses différents éléments === */
-            let newLine = document.createElement("div");
-                newLine.class = "ligne_equipe";
+            /* === On crée les lignes à ajouter et leurs différents éléments === */
+            /* === CAPITAINE === */
+            let newLine1 = document.createElement("div");
+            newLine1.class = "ligne_equipe";
+            newLine1.id = 0;
 
             /* Image de la couronne */
             let crown = document.createElement("img");
-                crown.id = "logo_crown";
-                crown.src = "../../img/logo_crown.png";
-                crown.alt = "logo";
-            
+            crown.id = "logo_crown";
+            crown.src = "../../img/logo_crown.png";
+            crown.alt = "logo";
+
             /* Nom Prénom */
-            let nom = document.createElement("span");
-                nom.class = "nom_teamMember";
-                nom.innerText = nom_prenom_cap;
+            let nom1 = document.createElement("span");
+            nom1.class = "nom_teamMember";
+            nom1.innerText = nom_prenom_cap;
 
             /* On insère les éléments dans la ligne */
-            newLine.appendChild(crown);
-            newLine.appendChild(nom);
+            newLine1.appendChild(crown);
+            newLine1.appendChild(nom1);
+
+
+            /* === MEMBRE 2 === */
+            let newLine2 = document.createElement("div");
+            newLine2.id = 1;
+            newLine2.onclick = "supprMemberTeam(1)"
+
+            /* Nom Prénom */
+            let nom2 = document.createElement("span");
+            nom2.class = "nom_teamMember";
+            nom2.id = idMembre;
+            nom2.innerText = membre;
+
+            /* On insère les éléments dans la ligne */
+            newLine2.appendChild(nom2);
+
+
+            /* === MEMBRE 3 === */
+            let newLine3 = document.createElement("div");
+            newLine3.id = 2;
+            newLine3.onclick = "supprMemberTeam(2)"
+
+            /* Nom Prénom */
+            let nom3 = document.createElement("span");
+            nom3.class = "nom_teamMember";
+            nom3.id = idMembre;
+            nom3.innerText = membre;
+
+            /* On insère les éléments dans la ligne */
+            newLine3.appendChild(nom3);
 
             /* On ajoute la ligne à l'équipe et on cache la ligne de création d'équipe */
             if (myTeam) {
-                myTeam.appendChild(newLine);
+                myTeam.appendChild(newLine1);
+                myTeam.appendChild(newLine2);
+                myTeam.appendChild(newLine3);
                 formEquipe.style.visibility = "hidden";
             }
         }
@@ -51,8 +97,12 @@ function CreateTeam(nom_prenom_cap) {
 
 }
 
-/* Ajout d'un membre dans une équipe */
-function addMemberTeam(idNewMember) {
+
+/* ================================== *
+*             AJOUT MEMBRE            *
+* =================================== */
+
+function addMemberTeam() {
     var xhr = getXHR();
 
     var membre = document.getElementById("participant").value;
@@ -64,47 +114,58 @@ function addMemberTeam(idNewMember) {
     xhr.onreadystatechange = function () {
 
         if (xhr.readyState == 4 && xhr.status == 200) {
-            let myTeam = document.getElementById("monEquipe");
+            var myTeam = document.getElementById("monEquipe");
             let listInscrits = document.getElementById("liste_participants");
             let memberToDelete = document.getElementsByName(idMembre);
 
             /* === On crée la ligne à ajouter et ses différents éléments === */
             let newLine = document.createElement("div");
-                newLine.class = "ligne_equipe";
-                /* On récupère le compteur du nb d'équipiers */
-                compteur = parseInt(myTeam.lastChild.id);
-                newLine.id = compteur+1;
-                newLine.onclick = "supprMemberTeam(" + compteur+1 + ")"
-            
+            newLine.class = "ligne_equipe";
+            /* On récupère le compteur du nb d'équipiers */
+            compteur = parseInt(myTeam.lastChild.id);
+            newLine.id = compteur + 1;
+            newLine.onclick = "supprMemberTeam(" + compteur + 1 + ")"
+
             /* Nom Prénom */
             let nom = document.createElement("span");
-                nom.class = "nom_teamMember";
-                nom.id = idMembre;
-                nom.innerText = membre;
+            nom.class = "nom_teamMember";
+            nom.id = idMembre;
+            nom.innerText = membre;
 
             /* On insère les éléments dans la ligne */
             newLine.appendChild(nom);
 
-            /* On ajoute le membre à l'équipe */
-            if (myTeam) {
-                myTeam.appendChild(newLine);
-            }
-            /* Et on le retire de la liste des participants disponibles */
-            if (listInscrits) {
-                listInscrits.removeChild(memberToDelete);
+            /* S'il y a moins de 8 membres dans l'équipe on ajoute le nouveau membre */
+            if (myTeam.childElementCount < 8) {
+
+                /* On ajoute le membre à l'équipe */
+                if (myTeam) {
+                    myTeam.appendChild(newLine);
+                }
+                /* Et on le retire de la liste des participants disponibles */
+                if (listInscrits) {
+                    listInscrits.removeChild(memberToDelete);
+                }
             }
         }
     }
 
     /* UPDATE DES VARS SESSIONS ET DE LA BDD */
-    xhr.open("POST", "gestionEquipe.php", true);
-    xhr.setRequestHeader('Content-Type',
-        'application/x-www-form-urlencoded;charset=utf-8');
-    xhr.send("val1=" + membre + "&type=ajout");
+
+    /* S'il y a moins de 8 membres dans l'équipe on effectue les modifications */
+    if (myTeam.childElementCount < 8) {
+        xhr.open("POST", "gestionEquipe.php", true);
+        xhr.setRequestHeader('Content-Type',
+            'application/x-www-form-urlencoded;charset=utf-8');
+        xhr.send("val1=" + membre + "&type=ajout");
+    }
 
 }
 
-/* Suppression d'un membre dans une équipe */
+/* ================================== *
+*         SUPPRESSION MEMBRE          *
+* =================================== */
+
 function supprMemberTeam(nb) {
     var xhr = getXHR();
 
@@ -117,32 +178,40 @@ function supprMemberTeam(nb) {
     xhr.onreadystatechange = function () {
 
         if (xhr.readyState == 4 && xhr.status == 200) {
-            let myTeam = document.getElementById("monEquipe");
+            var myTeam = document.getElementById("monEquipe");
             let listInscrits = document.getElementById("liste_participants");
             let memberToDelete = document.getElementsByClassName("ligne_equipe")[nb];
-            
+
             /* On crée la ligne à ajouter et ses différents éléments */
             let newOption = document.createElement("option");
-                newOption.value = membre;
-                newOption.name = idMembre;
+            newOption.value = membre;
+            newOption.name = idMembre;
 
-            /* On met le membre dans la liste des participants disponibles */
-            if (listInscrits) {
-                listInscrits.appendChild(newOption);
+            /* S'il y a plus de 3 membres dans l'équipe on supprime le membre */
+            if (myTeam.childElementCount > 3) {
+
+                /* On met le membre dans la liste des participants disponibles */
+                if (listInscrits) {
+                    listInscrits.appendChild(newOption);
+                }
+
+                /* Et on le retire de l'équipe */
+                if (myTeam) {
+                    myTeam.removeChild(memberToDelete);
+                }
             }
-
-            /* Et on le retire de l'équipe */
-            if (myTeam) {
-                myTeam.removeChild(memberToDelete);
-            }   
         }
     }
 
     /* UPDATE DES VARS SESSIONS ET DE LA BDD */
-    xhr.open("POST", "gestionEquipe.php", true);
-    xhr.setRequestHeader('Content-Type',
-        'application/x-www-form-urlencoded;charset=utf-8');
-    xhr.send("val1=" + membre + "&type=suppr" + "&idUser=" + idMembre);
+
+    /* S'il y a plus de 3 membres dans l'équipe on effectue les modificationss */
+    if (myTeam.childElementCount > 3) {
+        xhr.open("POST", "gestionEquipe.php", true);
+        xhr.setRequestHeader('Content-Type',
+            'application/x-www-form-urlencoded;charset=utf-8');
+        xhr.send("val1=" + membre + "&type=suppr" + "&idUser=" + idMembre);
+    }
 
 }
 
