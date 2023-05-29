41e212470d1a8070c7f52cb644ffc8d945f7846b
TitouanPrl <titouan.pradal@gmail.com>
2023-05-29 15:36:26 +0200

    ajout et gestion du quiz côté user

---
+++ php/User/envoiReponses.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+/* On inclut les fonctions de manipulation de la BDD */
+require_once("../../bdd/fonctionsBDD.php");
+
+/* On vérifie qu'un mdp a bien été rentré (évite qu'on dodge la page de connexion) */
+if (!isset($_SESSION["login"])){
+  header('Location:connexionInscription.php?message=1');
+  exit();
+}
+
+/* Si l'utilisateur n'est pas capitaine, on redirige vers la page d'équipe */
+if (!isset($_SESSION['infoTeam']) || ($_SESSION['capitaine'] == false)) {
+    header('Location: equipe.php');
+    exit();
+}
+
+/* On récupère le nombre de réponses */
+$nbReponses = (int) ($_POST['nbReponses']) + 1;
+
+/* On ajoute les réponses dans la BDD */
+for ($i = 0; $i < $nbReponses; $i++) {
+    addRéponse($conn, $_POST['id' . $i], $_SESSION['infoTeam']['idEquipe'], $_POST['reponse' . $i]);
+}
+
+/* On redirige vers l'accueil */
+header('Location: accueilUser.php'); 
+
+exit();
+
+?>
