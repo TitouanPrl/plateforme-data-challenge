21bfda6c342efd2baf39ca9b4461da77c1ba7a38
DEPRETER-Remi <depreterremi@gmail.com>
2023-06-02 08:51:35 +0200

    fautes d'orthographe

--- php/Admin/formSujet.php
+++ php/Admin/formSujet.php
@@ -8,7 +8,7 @@
   <main>
     <div style="display:flex; justify-content:center;align-items:center;padding-top:70px;">
             <div id="inscription">
-              <h2 class="titreForm" style="padding-bottom:0;"> Déposer un projet</h2>
+              <h2 class="titreForm" style="padding-bottom:0;"> Déposer un sujet</h2>
 
               <form action="VerifFormSujet.php" method="POST">
                 <div style="display:flex; justify-content:center;align-items:center">
